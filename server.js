0ed47eecf600fdb36378daaed0411365f9f0648f
Kevin Inman <kevin.inman@gmail.com>
2024-12-30 02:04:59 -0600

    Remove deprecated MongoDB connection options

--- server.js
+++ server.js
@@ -99,10 +99,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('MongoDB connection error:', err);
